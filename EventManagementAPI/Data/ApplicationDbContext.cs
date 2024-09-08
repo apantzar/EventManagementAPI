312cdf1360b0a14f4b8d926025dbce699aeddc75
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-09-08 20:37:29 +0300

    DbContext added

---
+++ EventManagementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EventManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace EventManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserEvent> UserEvents { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEvent>()
+            .HasKey(ue => new { ue.UserId, ue.EventId });
+
+            modelBuilder.Entity<UserEvent>()
+                .HasOne(ue => ue.User)
+                .WithMany(u => u.UserEvents)
+                .HasForeignKey(ue => ue.UserId);
+
+            modelBuilder.Entity<UserEvent>()
+                .HasOne(ue => ue.Event)
+                .WithMany(e => e.UserEvents)
+                .HasForeignKey(ue => ue.EventId);
+
+        }
+    }
+}
